540a32cf65bda5ce26ddf7bc21f216d01b917933
Craig Heckrath <checkrath@gmail.com>
2017-10-26 07:32:01 +0200

    initial project version

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild Microsoft.Bot.Sample.LuisBot.sln -p:DeployOnBuild=true -p:PublishProfile=CheckrathBRChatbot-Web-Deploy.pubxml -p:Password=exM4avCdAWczfN9mJFD4e6Jvvt9NRlAFiks8fbgl3sY5JHtM8LWiQ5ezo4tJ
+
