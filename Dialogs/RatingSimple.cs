4a0cb0cd949854936ba2d47bf4d431a5f478cd45
Craig Heckrath <checkrath@gmail.com>
2017-11-02 09:17:11 +0200

    New rating dialog

---
+++ Dialogs/RatingSimple.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LuisBot.Dialogs
+{
+    public class RatingSimple
+    {
+        //public async Task<int> CheckForRating()
+        //{
+
+        //}
+    }
+}
