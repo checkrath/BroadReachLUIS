6c36204dbc9164ccaa7b892e22767799f946a432
Craig Heckrath <checkrath@gmail.com>
2017-11-03 10:57:35 +0200

    Added DB calls and associated unit test

---
+++ BroadreachLuisTests/DBTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LuisBot.Data;
+using System.Collections.Generic;
+
+
+namespace BroadreachLuisTests
+{
+    [TestClass]
+    public class DBTest
+    {
+        [TestMethod]
+        public void TestBasicListFromDB()
+        {
+            //try create a new query and get back a list of districts
+            DataQuery query = new DataQuery();
+            List<string> districts= query.GetListOfDistricts("");
+            //check that the list has a couple of items
+            Assert.IsTrue(districts.Count > 1);
+
+
+        }
+    }
+}
---
+++ Data/DataQuery.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace LuisBot.Data
+{
+    
+    public class DataQuery
+    {
+        private const string DB_CONN= "Server=tcp:broadreachpoc.database.windows.net,1433;Initial Catalog=Checkrath_BroadreachPOC" +
+            ";Integrated Security=False;User Id=checkrath;Password=brIIM@gic.17;Encrypt=True;";
+        /// <summary>
+        /// Return a list of districts from the DB
+        /// </summary>
+        /// <param name="programName"></param>
+        public List<string> GetListOfDistricts(string programName)
+        {
+            List<string> districtList=new List<string>();
+            using (SqlConnection connection = new SqlConnection(DB_CONN))
+            {
+                using (SqlCommand cmd = new SqlCommand("SELECT distinct [DistrictName] FROM[dbo].[vw_DistrictsPerformance]", connection))
+                {
+                    //cmd.Parameters.AddWithValue("FirstName", firstName);
+                    connection.Open();
+                    using (var reader = cmd.ExecuteReader())
+                    {
+                        int x=reader.FieldCount;
+                        if (reader.HasRows)
+                        {
+                            while (reader.Read())
+                            {
+                                districtList.Add(reader.GetString(0));
+                               
+                            }
+                        }
+                    }
+                }
+            }
+
+            //Return a list of districts
+            return districtList;           
+
+        }
+    }
+}
